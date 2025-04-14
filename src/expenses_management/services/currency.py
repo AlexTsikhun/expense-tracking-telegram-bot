e71733251349fac3791aa01502af8feea95aa5c2
Oleksandr Tsikhun <sasacihun4@gmail.com>
2025-04-14 13:50:01 +0300

    fix: Right way to extract USD rate from `minfin.com.`

--- src/expenses_management/services/currency.py
+++ src/expenses_management/services/currency.py
@@ -7,5 +7,11 @@ class CurrencyService:
     def get_usd_rate() -> float:
         response = httpx.get("https://minfin.com.ua/currency/")
         soup = BeautifulSoup(response.text, "html.parser")
-        rate = float(float(soup.find("div", class_="sc-1x32wa2-9 bKmKjX").text.split(".")[0].replace(",", ".")))
+        rate_element = soup.find("div", class_="sc-1x32wa2-9 bKmKjX")
+        if not rate_element:
+            raise ValueError("Rate element not found on the page")
+        
+        rate_text = list(rate_element.stripped_strings)[0]
+        rate = float(rate_text.replace(",", "."))
+        
         return rate
